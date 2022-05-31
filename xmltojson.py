bd36d93aad24bbb7732d6dfa1a9c6278975f8b20
Richard Pajerski <richard.pajerski@braingu.com>
2022-05-31 14:45:54 -0400

    Working xmltodict script

--- xmltojson.py
+++ xmltojson.py
@@ -2,6 +2,6 @@ import json
 import xmltodict
 
 with open('payload.xml') as xml_payload:
-    data = xmltodict.parse(xml_payload)
+    data = xmltodict.parse(xml_payload.read())
 
     print(json.dumps(data))
